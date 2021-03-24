a4d49bcb15a9bbc5ed5a700dc530ed8c8e19a4ab
Antoine Duchâteau <ad@taktik.com>
2021-03-24 18:52:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('Garagedoor connected');
+    socket.on('disconnect', () => {
+        console.log('Garagedoor disconnected');
+    });
+});
+
+http.listen(PORT, () => {
+    console.log('listening on *:'+PORT);
+});
