5ea8df6fe211c98c256055cc5aa35aa954d5d8ef
Antoine Duchâteau <ad@taktik.com>
2021-04-18 12:22:25 +0200

    Improved server

--- index.js
+++ index.js
@@ -4,29 +4,41 @@ const io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000
 const SECRET = process.env.SECRET || 'S3l3n1umSh@z@m'
 
+const garageDoors = {}
+
 const validateToken = (token) => SECRET === token
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-
-function forward(action, token, callback) {
+function forward(action, uuid, token, callback) {
     if (validateToken(token)) {
-        io.emit(action, {ts: +new Date()}, (response) => {
+        garageDoors[uuid] && garageDoors[uuid].emit(action, {ts: +new Date()}, (response) => {
             callback({status: 200, response})
-        })
+        }) || callback({status: 404})
     } else {
-        callback({status: 403})
+        callback({status: 401})
     }
 }
 
 io.on('connection', (socket) => {
+    let uuids = []
     console.log('client connected');
-    socket.on('open', ({token}, callback) => forward('open', token, callback));
-    socket.on('close', ({token}, callback) => forward('close', token, callback));
-    socket.on('ping', ({token}, callback) => forward('ping', token, callback));
+    socket.on('register', ({token, uuid}, callback) => {
+        if (validateToken(token)) {
+            garageDoors[uuid] = socket
+            uuids.push(uuid)
+        } else {
+            callback({status: 401})
+        }
+    })
+    socket.on('open', ({token, uuid}, callback) => forward('open', uuid, token, callback));
+    socket.on('close', ({token, uuid}, callback) => forward('close', uuid, token, callback));
+    socket.on('ping', ({token, uuid}, callback) => forward('ping', uuid, token, callback));
+
     socket.on('disconnect', () => {
         console.log('client disconnected');
+        uuids.forEach(it => {delete garageDoors[it]})
     });
 });
 
