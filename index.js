912176f153db6004e84affd84d1429760501e6f9
Antoine Duchâteau <ad@taktik.com>
2021-04-18 11:38:20 +0200

    Improved server

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000
+const SECRET = process.env.SECRET || 'S3l3n1umSh@z@m'
 
+const validateToken = (token) => SECRET === token
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+
+function forward(action, token, callback) {
+    if (validateToken(token)) {
+        io.emit(action, {ts: +new Date()}, (response) => {
+            callback({status: 200, response})
+        })
+    } else {
+        callback({status: 403})
+    }
+}
+
 io.on('connection', (socket) => {
     console.log('client connected');
-    socket.on('click', (msg) => {
-        if (msg === 'S3l3n1umSh@z@m') {
-            io.emit('clicked', msg)
-        }
-    });
+    socket.on('open', ({token}, callback) => forward('open', token, callback));
+    socket.on('close', ({token}, callback) => forward('close', token, callback));
+    socket.on('ping', ({token}, callback) => forward('ping', token, callback));
     socket.on('disconnect', () => {
         console.log('client disconnected');
     });
